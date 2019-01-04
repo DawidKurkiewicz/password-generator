ca734c17fe731f458e0ffc66d9cd4b1eb5c965ad
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 15:23:31 +0100

    styles, js and html done

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.createElement("button")
+button.innerHTML = "generate 1000"
+document.body.appendChild(button)
+const container = document.createElement("section")
+document.body.appendChild(container)
