e5211df800955828a1149c9e376a3d01c3b2d80c
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 15:38:18 +0100

    passwords generator works

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.createElement("button")
-button.innerHTML = "generate 1000"
+button.innerHTML = "generate your password"
 document.body.appendChild(button)
 const container = document.createElement("section")
 document.body.appendChild(container)
@@ -10,12 +10,19 @@ const section = document.querySelector("section")
 
 const codesNumber = 1
 const charsNumber = 10
-
 const codeGen = () => {
+
     for (let i = 0; i < codesNumber; i++) {
         let code = ""
         for (let i = 0; i < charsNumber; i++) {
-const index = Math.floor(Math.random() * code.length)     
-   }
+            const index = Math.floor(Math.random() * chars.length)
+            code += chars[index]
+        }
+        container.innerHTML = ""
+        const div = document.createElement("div")
+        div.innerHTML = code
+        container.appendChild(div)
     }
 }
+
+btn.addEventListener("click", codeGen)
