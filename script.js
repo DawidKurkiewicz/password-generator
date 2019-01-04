865ffae1e031d187082b0b2e2cddf36944217048
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 15:28:49 +0100

    added html elements

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ button.innerHTML = "generate 1000"
 document.body.appendChild(button)
 const container = document.createElement("section")
 document.body.appendChild(container)
+
+const code = "ABCDEFGHIJKLMNOPRSTUWXYZ01234567890!@#$%^&*()"
+const btn = document.querySelector("button")
+const section = document.querySelector("section")
