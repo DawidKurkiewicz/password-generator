c4118eadb2eae4b09c7928ded7a1c4d7b9fd3875
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-04 15:33:56 +0100

    added function to generate code

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.body.appendChild(button)
 const container = document.createElement("section")
 document.body.appendChild(container)
 
-const code = "ABCDEFGHIJKLMNOPRSTUWXYZ01234567890!@#$%^&*()"
+const chars = "ABCDEFGHIJKLMNOPRSTUWXYZ01234567890!@#$%^&*()"
 const btn = document.querySelector("button")
 const section = document.querySelector("section")
+
+const codesNumber = 1
+const charsNumber = 10
+
+const codeGen = () => {
+    for (let i = 0; i < codesNumber; i++) {
+        let code = ""
+        for (let i = 0; i < charsNumber; i++) {
+const index = Math.floor(Math.random() * code.length)     
+   }
+    }
+}
